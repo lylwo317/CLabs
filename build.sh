b47d500c6a22794aa2024e4476c6d4a08b1eb74b
Kevin Xie <lylwo317@gmail.com>
2020-04-29 07:39:50 +0800

    build(build.sh): update build.sh

--- build.sh
+++ build.sh
@@ -9,4 +9,12 @@ cd ${buildDir}
 
 cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ..
 # cmake --build ${pwd}cmake-build-debug --target PerrorTest -- -j 16
-cmake --build . -- -j 16
+cores=`nproc`
+numberThread=`bc <<< "${cores}*2"`
+cmake --build . -- -j $numberThread
+
+cd ..
+
+if [[ -f ${buildDir}/compile_commands.json ]]; then
+    ln -sf ${buildDir}/compile_commands.json
+fi
