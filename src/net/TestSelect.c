c654f62c06449eaef59186869cc68c5e8121d7d5
xiejiahua <jiahua.xie@aqara.com>
2021-05-22 14:25:13 +0800

    add TestSelect.c

---
+++ src/net/TestSelect.c
@@ -0,0 +1,307 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define SERVER_PORT  12345
+
+#define TRUE             1
+#define FALSE            0
+
+int main(int argc, char *argv[])
+{
+    int    i, len, rc, on = 1;
+    int    listen_socket_fd, max_socket_fd, new_sd;
+    int    desc_ready, end_server = FALSE;
+    int    close_conn;
+    char   buffer[80];
+    struct sockaddr_in6 addr;
+    struct timeval      timeout;
+    fd_set              master_set, working_set;
+
+    /*************************************************************/
+    /* Create an AF_INET6 stream socket to receive incoming      */
+    /* connections on                                            */
+    /*************************************************************/
+    listen_socket_fd = socket(AF_INET6, SOCK_STREAM, 0);
+    if (listen_socket_fd < 0)
+    {
+        perror("socket() failed");
+        exit(-1);
+    }
+
+    /*************************************************************/
+    /* Allow socket descriptor to be reuseable                   */
+    /*************************************************************/
+    rc = setsockopt(listen_socket_fd, SOL_SOCKET, SO_REUSEADDR,
+                    (char *)&on, sizeof(on));
+    if (rc < 0)
+    {
+        perror("setsockopt() failed");
+        close(listen_socket_fd);
+        exit(-1);
+    }
+
+    /*************************************************************/
+    /* Set socket to be nonblocking. All of the sockets for      */
+    /* the incoming connections will also be nonblocking since   */
+    /* they will inherit that state from the listening socket.   */
+    /*************************************************************/
+    rc = ioctl(listen_socket_fd, FIONBIO, (char *)&on);//ioControl
+    if (rc < 0)
+    {
+        perror("ioctl() failed");
+        close(listen_socket_fd);
+        exit(-1);
+    }
+
+    /*************************************************************/
+    /* Bind the socket                                           */
+    /*************************************************************/
+    memset(&addr, 0, sizeof(addr));
+    addr.sin6_family      = AF_INET6;
+    memcpy(&addr.sin6_addr, &in6addr_any, sizeof(in6addr_any));
+    addr.sin6_port        = htons(SERVER_PORT);//host to network short
+    rc = bind(listen_socket_fd,
+              (struct sockaddr *)&addr, sizeof(addr));//将socket绑定到指定的ip和端口
+    if (rc < 0)
+    {
+        perror("bind() failed");
+        close(listen_socket_fd);
+        exit(-1);
+    }
+
+    /*************************************************************/
+    /* Set the listen back log                                   */
+    /*************************************************************/
+    rc = listen(listen_socket_fd, 32);//监听端口
+    if (rc < 0)
+    {
+        perror("listen() failed");
+        close(listen_socket_fd);
+        exit(-1);
+    }
+
+    /*************************************************************/
+    /* Initialize the master fd_set                              */
+    /*************************************************************/
+    FD_ZERO(&master_set);//clear a set
+    max_socket_fd = listen_socket_fd;
+    FD_SET(listen_socket_fd, &master_set);//add a given file descriptor from a set。后面select是对这个master_set进行检查
+
+    /*************************************************************/
+    /* Initialize the timeval struct to 3 minutes.  If no        */
+    /* activity after 3 minutes this program will end.           */
+    /*************************************************************/
+    timeout.tv_sec  = 3 * 60;
+    timeout.tv_usec = 0;
+
+    /*************************************************************/
+    /* Loop waiting for incoming connects or for incoming data   */
+    /* on any of the connected sockets.                          */
+    /*************************************************************/
+    do
+    {
+        /**********************************************************/
+        /* Copy the master fd_set over to the working fd_set.     */
+        /**********************************************************/
+        memcpy(&working_set, &master_set, sizeof(master_set));
+
+        /**********************************************************/
+        /* Call select() and wait 3 minutes for it to complete.   */
+        /**********************************************************/
+        printf("Waiting on select()...\n");
+        rc = select(max_socket_fd + 1, &working_set, NULL, NULL, &timeout);//检查是否有可读的文件描述符
+        //rc表示所有包含有事件的文件(readSet, writeSet, exceptSet)描述符的数量
+
+        /**********************************************************/
+        /* Check to see if the select call failed.                */
+        /**********************************************************/
+        if (rc < 0)
+        {
+            perror("  select() failed");
+            break;
+        }
+
+        /**********************************************************/
+        /* Check to see if the 3 minute time out expired.         */
+        /**********************************************************/
+        if (rc == 0)
+        {
+            printf("  select() timed out.  End program.\n");
+            break;
+        }
+
+        /**********************************************************/
+        /* One or more descriptors are readable.  Need to         */
+        /* determine which ones they are.                         */
+        /**********************************************************/
+        desc_ready = rc;
+        for (i=0; i <= max_socket_fd && desc_ready > 0; ++i)
+        {
+            /*******************************************************/
+            /* Check to see if this descriptor is ready            */
+            /*******************************************************/
+            if (FD_ISSET(i, &working_set))//检测fd(i)是否在set集合中，不在则返回0
+            {
+                /****************************************************/
+                /* A descriptor was found that was readable - one   */
+                /* less has to be looked for.  This is being done   */
+                /* so that we can stop looking at the working set   */
+                /* once we have found all of the descriptors that   */
+                /* were ready.                                      */
+                /****************************************************/
+                desc_ready -= 1;//检查完一个就减1
+
+                /****************************************************/
+                /* Check to see if this is the listening socket     */
+                /****************************************************/
+                if (i == listen_socket_fd)//如果是监听的fd，那就说明有新的连接进来了
+                {
+                    printf("  Listening socket is readable\n");
+                    /*************************************************/
+                    /* Accept all incoming connections that are      */
+                    /* queued up on the listening socket before we   */
+                    /* loop back and call select again.              */
+                    /*************************************************/
+                    do
+                    {
+                        /**********************************************/
+                        /* Accept each incoming connection.  If       */
+                        /* accept fails with EWOULDBLOCK, then we     */
+                        /* have accepted all of them.  Any other      */
+                        /* failure on accept will cause us to end the */
+                        /* server.                                    */
+                        /**********************************************/
+                        //accept():
+                        //在non-blocking模式下，如果返回值为-1，且errno == EAGAIN或errno == EWOULDBLOCK表示no connections没有新连接请求；
+//                        EAGAIN or EWOULDBLOCK
+//                        The socket is marked nonblocking and no connections are present to be accepted.  POSIX.1-2001 and POSIX.1-2008 allow either error to be returned for  this
+//                        case, and do not require these constants to have the same value, so a portable application should check for both possibilities.
+                        new_sd = accept(listen_socket_fd, NULL, NULL);//这里面其实是一个队列，所以循环去获取直到new_sd == -1为止
+                        if (new_sd < 0)
+                        {
+                            if (errno != EWOULDBLOCK)
+                            {
+                                perror("  accept() failed");
+                                end_server = TRUE;
+                            }
+                            break;
+                        }
+
+                        /**********************************************/
+                        /* Add the new incoming connection to the     */
+                        /* master read set                            */
+                        /**********************************************/
+                        printf("  New incoming connection - %d\n", new_sd);
+                        FD_SET(new_sd, &master_set);
+                        if (new_sd > max_socket_fd)
+                            max_socket_fd = new_sd;
+
+                        /**********************************************/
+                        /* Loop back up and accept another incoming   */
+                        /* connection                                 */
+                        /**********************************************/
+                    } while (new_sd != -1);//处理下一个连接
+                }
+
+                /****************************************************/
+                /* This is not the listening socket, therefore an   */
+                /* existing connection must be readable             */
+                /****************************************************/
+                else
+                {//说明不是listen_socket_fd收到新的连接请求，而是其它已经与服务器建立的连接有可读数据
+                    printf("  Descriptor %d is readable\n", i);
+                    close_conn = FALSE;
+                    /*************************************************/
+                    /* Receive all incoming data on this socket      */
+                    /* before we loop back and call select again.    */
+                    /*************************************************/
+                    do
+                    {
+                        /**********************************************/
+                        /* Receive data on this connection until the  */
+                        /* recv fails with EWOULDBLOCK.  If any other */
+                        /* failure occurs, we will close the          */
+                        /* connection.                                */
+                        /**********************************************/
+                        rc = recv(i, buffer, sizeof(buffer), 0);//读取数据
+                        if (rc < 0)
+                        {
+                            if (errno != EWOULDBLOCK)
+                            {
+                                perror("  recv() failed");
+                                close_conn = TRUE;
+                            }
+                            break;
+                        }
+
+                        /**********************************************/
+                        /* Check to see if the connection has been    */
+                        /* closed by the client                       */
+                        /**********************************************/
+                        if (rc == 0)//连接被关闭
+                        {
+                            printf("  Connection closed\n");
+                            close_conn = TRUE;
+                            break;
+                        }
+
+                        /**********************************************/
+                        /* Data was received                          */
+                        /**********************************************/
+                        len = rc;
+                        printf("  %d bytes received\n", len);
+
+                        /**********************************************/
+                        /* Echo the data back to the client           */
+                        /**********************************************/
+                        rc = send(i, buffer, len, 0);//原样返回数据
+                        if (rc < 0)
+                        {
+                            perror("  send() failed");
+                            close_conn = TRUE;
+                            break;
+                        }
+
+                    } while (TRUE);
+
+                    /*************************************************/
+                    /* If the close_conn flag was turned on, we need */
+                    /* to clean up this active connection.  This     */
+                    /* clean up process includes removing the        */
+                    /* descriptor from the master set and            */
+                    /* determining the new maximum descriptor value  */
+                    /* based on the bits that are still turned on in */
+                    /* the master set.                               */
+                    /*************************************************/
+                    if (close_conn)
+                    {
+                        close(i);
+                        FD_CLR(i, &master_set);
+                        if (i == max_socket_fd)
+                        {
+                            while (FD_ISSET(max_socket_fd, &master_set) == FALSE)
+                                max_socket_fd -= 1;
+                        }
+                    }
+                } /* End of existing connection is readable */
+            } /* End of if (FD_ISSET(i, &working_set)) */
+        } /* End of loop through selectable descriptors */
+
+    } while (end_server == FALSE);
+
+    /*************************************************************/
+    /* Clean up all of the sockets that are open                 */
+    /*************************************************************/
+    for (i=0; i <= max_socket_fd; ++i)
+    {
+        if (FD_ISSET(i, &master_set))
+            close(i);
+    }
+}
