2ab291baf19dcb4d92f6d18ddace4d18a4bd275b
KevinXie <lylwo317@gmail.com>
2021-06-15 09:12:09 +0800

    添加decltype smartPointer测试代码

--- src/cpp/TestDecltype.cpp
+++ src/cpp/TestDecltype.cpp
@@ -2,12 +2,22 @@
 // Created by kevin on 2021/6/14.
 //
 #include <bits/move.h>
+#include <typeinfo>
 int getSum(){
     return 10;
 }
 const int&& fx(){
 
 }
+
+class Person{
+private:
+    int age = 0;
+public:
+    int getAge(){
+        return age;
+    }
+};
 
 
 int main(int argc, const char *argv[]){
@@ -16,17 +26,32 @@ int main(int argc, const char *argv[]){
     int i = 3;
     int j = 3;
     decltype(j) i2 = j;    //i2类型 int
+    //特例1：
+    //通过(变量)让程序员可以获取该类型的引用类型
     decltype((j)) i3 = j;  //i3类型 int&，()变成引用类型
+
     int &k = j;
-    decltype(k+1) i4;  //i4类型 int
+    decltype(k) k1 = j;  //k1类型 int&
+
+    const int &h = j;
+    decltype(h) h1 = h;  //h1类型 const int&
 
     decltype(fx()) i6 = 30;//i6 const int&&;
+    decltype((getSum())) i7 = 30;//i7 int;
 
-    const int &n = j;
-    decltype(n) i1 = n;    //i1类型const int&
+    int * m = &k;
+    decltype(m) m1 = m;    //m1类型int*
+    //特例2:
+    //为什么解引用指针，得到的是引用类型？
+    //因为解引用后得到指针的对象，并能向该对象赋值，这个时候只有引用类型能做到
+    //否则如果是普通int类型，那就只是拷贝了。
+    decltype(*m) m2 = *m;    //m2类型int&
+    m2 = 30;//m也会变成30
+    //decltype(*m) m2 = *m;//假如m2类型是int，之后对m2赋值并无法改变m的值
 
-    const int * const m = &k;
-    decltype(m) i5 = m;    //i5类型const int&
+    const int * const n = &k;
+    decltype(n) n1 = n;    //n1类型const int* const
+    decltype(*n) n2 = *n;    //n2类型const int&，引用本身隐含了顶层const
 
     return 0;
 }
---
+++ src/cpp/TestSmartPointer.cpp
@@ -0,0 +1,101 @@
+//
+// Created by kevin on 2021/6/14.
+//
+#include <iostream>
+#include <memory>
+//#define INSIGHTS_USE_TEMPLATE
+
+using namespace std;
+
+class Person {
+	int m_age;
+public:
+	Person() {
+		cout << "Person()" << endl;
+	}
+	Person(int age) :m_age(age) {
+		cout << "Person(int)" << endl;
+	}
+	~Person() {
+		cout << "~Person()" << endl;
+	}
+	void run() {
+		cout << "run() - " << m_age << endl;
+	}
+};
+
+/* First instantiated from: insights.cpp:227 */
+#ifdef INSIGHTS_USE_TEMPLATE
+template<>
+    class auto_ptr<Person>
+    {
+
+      private:
+      Person * _M_ptr;
+
+      public:
+      using element_type = Person;
+      inline explicit auto_ptr(element_type * __p)
+      : _M_ptr{__p}
+      {
+      }
+
+      inline auto_ptr(auto_ptr<Person> & __a);
+
+      template<typename _Tp1>
+      inline auto_ptr(auto_ptr<_Tp1> & __a);
+
+      inline auto_ptr<Person> & operator=(auto_ptr<Person> & __a);
+
+      template<typename _Tp1>
+      inline auto_ptr<Person> & operator=(auto_ptr<_Tp1> & __a);
+      inline ~auto_ptr() noexcept
+      {
+        delete this->_M_ptr;
+      }
+
+      inline element_type & operator*() const;
+
+      inline element_type * operator->() const;
+
+      inline element_type * get() const;
+
+      inline element_type * release();
+
+      inline void reset(element_type * __p);
+
+      inline auto_ptr(std::auto_ptr_ref<Person> __ref);
+
+      inline auto_ptr<Person> & operator=(std::auto_ptr_ref<Person> __ref);
+
+    };
+
+#endif
+
+
+/**
+ * 不能共享
+ */
+void testAutoPointer(){
+    auto_ptr<Person> p(new Person);
+}
+
+void testSharedPointer(){
+    shared_ptr<Person> p(new Person);
+    shared_ptr<Person[]> pArray(new Person[3]);
+    shared_ptr<Person> p2 = p;//拷贝，引用计数+1
+}
+void testUniquePointer(){
+    unique_ptr<Person> p(new Person);
+    unique_ptr<Person[]> pArray(new Person[3]);
+    unique_ptr<Person> p2(std::move(p));//转移
+
+}
+
+int main()
+{
+//    testAutoPointer();
+//    testSharedPointer();
+    testUniquePointer();
+    return 0;
+}
