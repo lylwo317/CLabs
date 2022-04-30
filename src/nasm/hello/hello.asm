cf71738a13b5183b865d8bb06f08e960dd9240ef
KevinXie <lylwo317@gmail.com>
2022-04-30 11:52:46 +0800

    添加 nasm hello

---
+++ src/nasm/hello/hello.asm
@@ -0,0 +1,19 @@
+section .data ;数据端，放常量之类的
+    sys_write_code equ 1;声明常量，int sys_write_code = 1
+    sys_exit_code equ 60
+    msg db "hello, world!",`\n`
+
+section .text ;代码段
+    global _start
+
+;https://filippo.io/linux-syscall-table/
+_start:
+    mov rax, sys_write_code;syscall table index 1 == sys_write
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 14
+    syscall;sys_write(fd:1, const char *buf:msg, size_t: 14)
+
+    mov rax, sys_exit_code;syscall table index 60 == sys_exit
+    mov rdi, 0;
+    syscall;sys_exit(0)
